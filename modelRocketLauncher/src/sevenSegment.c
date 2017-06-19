c8cd3cf3a7c856c7c55e36febd9b283d54eb039b
Isaac Rose <isaacdrose@gmail.com>
2017-06-18 22:54:35 -0400

    7-segment based on physical connections

--- modelRocketLauncher/src/sevenSegment.c
+++ modelRocketLauncher/src/sevenSegment.c
@@ -11,6 +11,18 @@
  * PA6 - g
  */
 
+/*
+ * Connections
+ * char_LED	PA
+ * 		g	0
+ * 		f	1
+ * 		a	2
+ * 		b	3
+ * 		c	4
+ * 		d	5
+ * 		e	6
+ */
+
 void initSevenSegment(){
 	 // Enable GPIO Peripheral clock
 	int index = 0;
@@ -34,37 +46,37 @@ void updateSevenSegment(unsigned int value){
 
 	switch(value){
 		case 0:
-			character = 0b00000000;
+			character = 0x7e;
 			break;
 		case 1:
-			character = 0b00000000;
+			character = 0x18;
 			break;
 		case 2:
-			character = 0b00000000;
+			character = 0x6d;
 			break;
 		case 3:
-			character = 0b00000000;
+			character = 0x3d;
 			break;
 		case 4:
-			character = 0b00000000;
+			character = 0x1b;
 			break;
 		case 5:
-			character = 0b00000000;
+			character = 0x37;
 			break;
 		case 6:
-			character = 0b00000000;
+			character = 0x77;
 			break;
 		case 7:
-			character = 0b00000000;
+			character = 0x1c;
 			break;
 		case 8:
-			character = 0b00000000;
+			character = 0x7f;
 			break;
 		case 9:
-			character = 0b00000000;
+			character = 0x1f;
 			break;
 		default:
-			character = 0b00000000;
+			character = 0x01; //-
 			break;
 	}
 
