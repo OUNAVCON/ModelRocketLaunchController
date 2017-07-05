93af9a598c323518e49c107eef44409a171ae0e6
Isaac Rose <isaacdrose@gmail.com>
2017-07-05 13:35:57 -0400

    Added image of 7-segment display and main.h header file.

---
+++ modelRocketLauncher/src/main.h
@@ -0,0 +1,17 @@
+/*
+ * main.h
+ *
+ *  Created on: Jul 4, 2017
+ *      Author: Isaac.Rose
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+typedef enum {
+	STATE_ARMED,
+	STATE_LAUNCH,
+	STATE_RESET
+}states_t;
+
+#endif /* MAIN_H_ */
